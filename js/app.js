262c89c23d8c4f152e17a32377f107246a3a57d6
naylson <naylsonrj@gmail.com>
2022-11-06 01:16:50 -0300

    usando fadeout

--- js/app.js
+++ js/app.js
@@ -1,9 +1,39 @@
-$(".abrir-modal").click(function () {
-  $(".modal").toggleClass("active");
-  var condicao = $(".modal").hasClass("active");
-  if (condicao) {
-    $(this).html("Fechar");
-  } else {
-    $(this).html("Abrir");
-  }
+// 
+// $(".abrir-modal").click(function () {
+//   $(".modal").toggleClass("active");
+//   var condicao = $(".modal").hasClass("active");
+//   if (condicao) {
+//     $(this).html("Fechar");
+//   } else {
+//     $(this).html("Abrir");
+//   }
+// });
+
+// animação do menu
+/*
+$('a').click(function(){
+  $('.modal')
+  .animate({
+    opacity: 'toggle',
+    height: 'toggle',    
+  }, 500);  
 });
+*/
+
+
+// efeito fade
+$('.btn').click(function(){
+  $('.modal').fadeOut();
+//   $('.modal').hide(500);
+  });
+$('.abrir-modal').click(function(){
+  $('.modal').fadeIn();
+});
+
+
+
+
+
+
+
+
